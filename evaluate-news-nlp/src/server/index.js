2b936785b65f3ec502b2b77441698129850f1058
Foystor <foystor@gmail.com>
2023-01-11 22:49:28 +0800

    feat: Use external MeaningCloud API

--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -1,12 +1,18 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const dotenv = require('dotenv')
 dotenv.config()
 
-const apiKey = process.env.API_KEY
-
 const app = express()
+
+// middle-ware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
 app.use(express.static('dist'))
 
@@ -18,10 +24,26 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    const formdata = new FormData()
+    formdata.append('key', process.env.API_KEY)
+    formdata.append('url', req.query.url)
+    formdata.append('lang', 'auto')
+
+    const requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    }
+
+    fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        res.send(data)
+    })
+    .catch(error => console.log('error', error))
 })
