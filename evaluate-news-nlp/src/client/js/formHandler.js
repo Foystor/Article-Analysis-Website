eb791d40b209a6bc43afef6ad499ba77e77e7829
Foystor <foystor@gmail.com>
2023-01-11 23:19:06 +0800

    feat: Update UI with API response

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -10,7 +10,15 @@ function handleSubmit(event) {
     .then(res => res.json())
     .then(res => {
         console.log(res)
+        const random = getRndInteger(res.sentence_list.length -1)
+        document.getElementById('subjectivity').textContent = `Subjectivity: ${res.subjectivity.toLowerCase()}`
+        document.getElementById('snippet').textContent = `Text Snippet: ${res.sentence_list[random].text}`
     })
+}
+
+// return a random number between 0 and max (both included)
+function getRndInteger(max) {
+    return Math.floor(Math.random() * (max + 1))
 }
 
 export { handleSubmit }
